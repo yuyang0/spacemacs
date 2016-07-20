69507b9269abff053264c3cb3dd8a5357298eb12
YuYang <yy2012cn@gmail.com>
2016-07-20 15:24:29 +0800

    more templates

--- private/my-settings/yatemplates/13:.*.h
+++ private/my-settings/yatemplates/13:.*.h
@@ -1,3 +1,4 @@
+/* -*- Mode: C; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
 /*
  * ==================================================================
  *       Filename:  `(file-name-nondirectory buffer-file-name)`
